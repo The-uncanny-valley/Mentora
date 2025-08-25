130f6e9c4875c6f97dec319d087c4d04db694b79
the-uncanny-valley <runcannyvalley@gmail.com>
2025-08-25 20:12:18 +0300

    chore(deps): add Retrofit networking dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
     implementation(libs.room.runtime)
     annotationProcessor(libs.androidx.room.compiler)
 
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
