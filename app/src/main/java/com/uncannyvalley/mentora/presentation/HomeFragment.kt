20fbcd917762acebfbce826db74efe201ee3e9f6
the-uncanny-valley <runcannyvalley@gmail.com>
2025-08-27 22:25:19 +0300

    feat(presentation): add HomeFragment with ViewModel observation and course list

--- app/src/main/java/com/uncannyvalley/mentora/presentation/HomeFragment.kt
+++ app/src/main/java/com/uncannyvalley/mentora/presentation/HomeFragment.kt
@@ -1,7 +1,52 @@
 package com.uncannyvalley.mentora.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.uncannyvalley.mentora.R
+import com.uncannyvalley.mentora.presentation.adapter.CourseAdapter
+import com.uncannyvalley.mentora.presentation.viewmodel.CourseViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
+    private val viewModel: CourseViewModel by viewModel()
+    private lateinit var adapter: CourseAdapter
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate layout
+        return inflater.inflate(
+            R.layout.fragment_home,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_courses)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter = CourseAdapter(
+            // TODO: handle learn more
+            onLearnMoreClick = { course -> {} },
+            onFavouriteClick = { course ->
+                viewModel.toggleLike(course)
+            }
+        )
+
+        recyclerView.adapter = adapter
+
+        viewModel.courses.observe(viewLifecycleOwner) { courses ->
+            adapter.submitList(courses)
+        }
+
+        viewModel.loadCourses()
+    }
 }
