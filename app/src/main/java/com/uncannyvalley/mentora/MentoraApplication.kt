ca26bf2ea3571a20e42917d666999b02c9adaeaf
the-uncanny-valley <runcannyvalley@gmail.com>
2025-08-27 22:21:09 +0300

    chore(app): initialize Koin in MentoraApplication and update manifest

---
+++ app/src/main/java/com/uncannyvalley/mentora/MentoraApplication.kt
@@ -0,0 +1,26 @@
+package com.uncannyvalley.mentora
+
+import android.app.Application
+import com.uncannyvalley.mentora.di.networkModule
+import com.uncannyvalley.mentora.di.repositoryModule
+import com.uncannyvalley.mentora.di.useCaseModule
+import com.uncannyvalley.mentora.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class MentoraApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Start Koin
+        startKoin {
+            androidContext(this@MentoraApplication)
+            modules(listOf(
+                networkModule,
+                repositoryModule,
+                useCaseModule,
+                viewModelModule)
+            )
+        }
+    }
+}
